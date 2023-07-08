0bc9d4cb30f936512d88c6c6b267bfb26115bdc3
Tomas Zagorskis <tomzagos@gmail.com>
2023-07-08 10:09:00 +0300

    set up configs, dependencies

---
+++ index.ts
@@ -0,0 +1,7 @@
+import { httpServer } from './src/http_server/index';
+import 'dotenv/config.js';
+
+const HTTP_PORT = process.env.HTTP_PORT || 8181;
+
+console.log(`Start static http server on the ${HTTP_PORT} port!`);
+httpServer.listen(HTTP_PORT);
