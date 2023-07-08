53224d9218bcb7b5b3ba7c672eb7757d52084350
Tomas_Zagorskis <tomzagos@gmail.com>
2023-07-08 22:31:12 +0300

    feat: add winner controller

---
+++ src/controller/winnerController.ts
@@ -0,0 +1,24 @@
+import { Winner } from '../types/type';
+
+class WinnersController {
+  private winners: Winner[] = [];
+
+  getWinner(name: string) {
+    return this.winners.find((winner) => winner.name === name);
+  }
+
+  getWinners() {
+    return this.winners;
+  }
+
+  updateWin(name: string) {
+    const winner = this.getWinner(name);
+    if (winner) {
+      winner.wins++;
+      return;
+    }
+    this.winners.push({ name, wins: 1 });
+  }
+}
+
+export const winners = new WinnersController();
