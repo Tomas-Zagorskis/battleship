d02c2768f7fda3a34aa2ab1825f5ffc377b1677a
Tomas Zagorskis <tomzagos@gmail.com>
2023-07-08 12:48:27 +0300

    feat: proper way to disconnect

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,15 @@ console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
 
 console.log(`Start WebSocket server on the ${WS_PORT} port!`);
-webSocketServer(WS_PORT);
+const wss = webSocketServer(WS_PORT);
+
+process.on('SIGINT', () => {
+  console.log('Shutting down http and websocket servers!');
+  wss.close();
+  wss.clients.forEach((client) => {
+    client.close();
+  });
+  httpServer.close();
+  httpServer.closeAllConnections();
+  process.exit();
+});
